e3bd0ff58532b919d2117b2cf970d5181bdc8dbd
Tony Ayda <tonyayda7@gmail.com>
2025-01-04 12:20:01 +0200

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,19 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
+import { env } from './common/utils/envConfig';
+import { app, logger } from './server';
 
-dotenv.config();
-
-const app: Application = express();
-const port = process.env.PORT || 3000;
-
-
-app.use(cors());
-app.use(helmet());
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Pomodoro backend is running!");
+const server = app.listen(env.PORT, () => {
+  const { NODE_ENV, HOST, PORT } = env;
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
 });
 
+const onCloseSignal = () => {
+  logger.info('sigint received, shutting down');
+  server.close(() => {
+    logger.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+process.on('SIGINT', onCloseSignal);
+process.on('SIGTERM', onCloseSignal);
